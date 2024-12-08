e54753ae1f492ffebf88e597e71f382fb10eb53e
Vitalii <vitalii.arts@gmail.com>
2024-12-09 00:16:32 +0100

    lesson 4

--- public/index.php
+++ public/index.php
@@ -1,9 +1,11 @@
 <?php
 
-echo "lesson 3 - ok";
+echo "lesson 4 - ok";
 
 if(PHP_MAJOR_VERSION < 8) {
     exit("You need version PHP more or equal 8.0");
 }
 
 require_once dirname(__DIR__) . '/config/init.php';
+
+new \core\App();
